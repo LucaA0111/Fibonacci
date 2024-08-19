4febb5f578838eb40927421c3614a115680340d0
LucaA0111 <luca.acquati10@gmail.com>
2024-08-19 18:51:58 +0200

    Fibonacci sequence java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,21 +4,35 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
+    static ArrayList<BigInteger> sequence;
+
     //Used BigInteger to avoid overflow
     public static ArrayList<BigInteger> fibonacci(int lunghezza) {
-        ArrayList<BigInteger> succesione = new ArrayList<>();
-        succesione.add(0, BigInteger.ZERO);
-        succesione.add(1, BigInteger.ONE);
 
-        for(int i =2; i < lunghezza; i++){
-            BigInteger x = succesione.get(i-2);
-            BigInteger y =  succesione.get(i-1);
-            BigInteger sum = x.add(y);
-            succesione.add(i, sum);
-            System.out.println(succesione);
+        sequence = new ArrayList<>();
+        if (lunghezza <= 0) {
+            return null;
+
+        } else if (lunghezza==1) {
+            sequence.add(0, BigInteger.ZERO);
+            System.out.println(sequence);
+            return sequence;
+
+        }else {
+
+            sequence.add(0, BigInteger.ZERO);
+            sequence.add(1, BigInteger.ONE);
+
+            for (int i = 2; i < lunghezza; i++) {
+                BigInteger x = sequence.get(i - 2);
+                BigInteger y = sequence.get(i - 1);
+                BigInteger sum = x.add(y);
+                sequence.add(i, sum);
+                System.out.println(sequence);
+            }
         }
 
-        return succesione;
+        return sequence;
     }
 
     public static void main(String[] args) {
